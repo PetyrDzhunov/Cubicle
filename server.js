4441651f2b27bf36f1655c83927de2615c8e4dc0
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 13:20:01 +0300

    Add routes

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const config = require('./config/config');
 const app = express();
+const routes = require('./routes');
 
 require('./config/express')(app); // returns a function which we pass our app as a parameter and invoke it 
-
-
-app.get('/', (req, res) => {
-    res.render('home', { layout: false })
-})
+app.use(routes); // routes-middleware;
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
