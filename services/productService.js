d5d1a2f6005b8a8441f2e207ded871a6d80b25c4
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 16:57:06 +0300

    Resolve async file write

--- services/productService.js
+++ services/productService.js
@@ -8,15 +8,13 @@ function getAll() {
     return productsData;
 };
 
-function createProduct(data) {
+function createProduct(data, callback) {
     let cube = new Cube(uniqid(), data.name, data.description, data.imageUrl, data.difficultyLevel);
     productsData.push(cube);
-    fs.writeFile(path.join(__dirname + '../config/products.json'), JSON.stringify(productsData), (err) => {
-        if (err) {
-            console.log(err);
-            return;
-        };
-    });
+    fs.writeFile(
+        path.join(__dirname + '../config/products.json'),
+        JSON.stringify(productsData),
+        callback)
 };
 
 function getOne(id) {
