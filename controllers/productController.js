fb9f7e15c54e9047daf5c392d0cc1cc6e66c05df
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 14:30:20 +0300

    Add page titles to layout

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,10 +4,10 @@ const router = Router();
 // 
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: 'Homepage' });
 });
 
 router.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create cube' });
 })
 module.exports = router;
