48f06bc701a013bd3226e6bf3e401bc238143e01
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 15:21:02 +0300

    Parse form data

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,8 +11,17 @@ router.get('/create', (req, res) => {
     res.render('create', { title: 'Create cube' });
 });
 
+router.post('/create', (req, res) => {
+    console.log('created');
+    console.log(req.body); // tova koeto klienta e izpratil;
+
+    res.send('created');
+});
+
 router.get('/details/:productId', (req, res) => {
     console.log(req.params.productId);
     res.render('details', { title: 'Product Details' })
-})
+});
+
+
 module.exports = router;
