bae2a5f085d4e522c20225d07178291375efaf02
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 13:11:43 +0300

    extract express config

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-
 const config = require('./config/config');
-
 const app = express();
 
-app.engine('hbs', handlebars({ extname: 'hbs' }));
-app.set('view engine', 'hbs');
+require('./config/express')(app); // returns a function which we pass our app as a parameter and invoke it 
 
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home', { layout: false })
 })
-
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
