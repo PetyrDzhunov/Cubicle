ecff8cbb1407d50c935cbf67ecae7a08097770e3
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 14:18:11 +0300

    Add not found page

--- routes.js
+++ routes.js
@@ -4,8 +4,17 @@ const aboutController = require('./controllers/aboutController');
 const router = Router();
 router.use('/', productController);
 router.use('/about', aboutController)
-    //iskam na vsichki zaqvki koito idvat na /product ,
-    // da gi naznachish na nashiq product contoller
+
+
+// if none of those routers cant handle this path =>
+// we dont have a controller for it => it does not exist
+// =>  we cant resolve it , so for any else endpoint,
+// => render 404.
+router.get('*', (req, res) => {
+    res.render('404', { layout: false });
+});
+//iskam na vsichki zaqvki koito idvat na /product ,
+// da gi naznachish na nashiq product contoller
 
 
 module.exports = router;
