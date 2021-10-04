49654315e0a18b02cb6c39e4a17840fbbe285b82
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-04 14:15:49 +0300

    Add about controller

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-
+const productController = require('./controllers/productController');
+const aboutController = require('./controllers/aboutController');
 const router = Router();
+router.use('/', productController);
+router.use('/about', aboutController)
+    //iskam na vsichki zaqvki koito idvat na /product ,
+    // da gi naznachish na nashiq product contoller
 
-router.get('/', (req, res) => {
-    res.render('home', { layout: false })
-});
 
 module.exports = router;
